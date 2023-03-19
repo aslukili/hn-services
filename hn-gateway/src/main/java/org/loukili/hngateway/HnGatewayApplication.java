6bbc4eba0414bcbb568e5814afe2fdb04eb59b48
Abdeslam Loukili <abdeslam.edu@gmail.com>
2023-03-19 10:22:13 +0000

    init gateway

---
+++ hn-gateway/src/main/java/org/loukili/hngateway/HnGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.loukili.hngateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HnGatewayApplication.class, args);
+    }
+
+}
---
+++ hn-gateway/src/test/java/org/loukili/hngateway/HnGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.loukili.hngateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
